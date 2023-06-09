3d64a46a0203a0320e509ba6b707facd3138d5ed
Bruno Teixeira <101830997+bttex@users.noreply.github.com>
2023-06-08 21:24:52 -0300

    Added bot file

---
+++ tel_bot.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import telebot
+import time
+
+# URL da página web
+url = "http://ubistatic-a.akamaihd.net/0115/tctd2/status.html"
+
+# Obtendo o conteúdo da página web
+
+# Inicializando o bot do Telegram
+bot_token = "6010277135:AAHqSfHKnZPChtmCzScXd8aadZ8bF1R_LNw"
+bot = telebot.TeleBot(bot_token)
+
+
+@bot.message_handler(commands=['status'])
+def check_status(message):
+    # Obtendo o conteúdo da página web
+    response = requests.get(url)
+    html_content = response.text
+
+    # Analisando o HTML com BeautifulSoup
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # Verificando se a imagem de manutenção está presente
+    div_pc = soup.find("div", {"id": "dvPC"})
+    image_pc = div_pc.find("img", {"id": "maintenancePC"}) if div_pc else None
+
+    if image_pc:
+        # Enviando mensagem para o chat
+        chat_id = message.chat.id
+        response_message = "Servidor se encontra em manutenção"
+        bot.send_message(chat_id, response_message)
+    else:
+        response_message = "A manutenção acabou!."
+        bot.send_message(message.chat.id, response_message)
+
+
+bot.polling()
